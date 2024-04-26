0abccf9b136fd8b3e5996454ba2895bd650fe958
Hosni-24bey <hosni.trabelsi@rmsoftware.de>
2024-04-26 15:13:14 +0100

    fixes
    rspnsv fixes + navigation fixes

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -145,7 +145,7 @@ export default function Home() {
           </div>
         </div> */}
 
-        <section class="landingpage">
+        <section class="landingpage" id='home'>
           <div class="container d-flex align-items-center">
             <div class="lp-alignment">
               <img
