bb9e16c8618e17f1df648c03be47355bc3021eb8
wafa <oueslatiwafa454@gmail.com>
2024-05-17 17:05:35 +0100

    commit

--- client/src/Dummy/AppData.js
+++ client/src/Dummy/AppData.js
@@ -36,7 +36,7 @@ export const apartmentList = [
       "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978163",
     verification:
       "1399b10e610ab6085c0d7e4ef3fe7438637e9d61583da543793e346b3a05ebd2",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     bedroom: 3,
     bathroom: 2,
     images: [
@@ -48,12 +48,12 @@ export const apartmentList = [
       { image_url: pent6 },
     ],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
   {
     id: "smoobuApartment1978148de",
     name: "Townloft - am Borussia Park -L1-",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     loadCalendarUrl:
       "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978148",
     verification:
@@ -62,17 +62,19 @@ export const apartmentList = [
     parking: 1,
     images: [{ image_url: "https://picsum.photos/200/300" }],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
   {
     id: "smoobuApartment1978151de",
     name: "Townloft - am Borussia Park -L2-",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     bedroom: 3,
     bathroom: 2,
-    loadCalendarUrl:"https://login.smoobu.com/de/cockpit/widget/single-calendar/1978154",
-        verification:"0e509e6d2f2584f4adaa79f39c03117f20a04cdf47f9a97bad2ba18caa6a7b04",
-  
+    loadCalendarUrl:
+      "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978154",
+    verification:
+      "0e509e6d2f2584f4adaa79f39c03117f20a04cdf47f9a97bad2ba18caa6a7b04",
+
     images: [
       { image_url: L2 },
       { image_url: L21 },
@@ -82,16 +84,18 @@ export const apartmentList = [
       { image_url: L21212 },
     ],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
   {
     id: "smoobuApartment1978154de",
     name: "Townloft - am Borussia Park -L3-",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     bedroom: 3,
     bathroom: 2,
-    loadCalendarUrl:"https://login.smoobu.com/de/cockpit/widget/single-calendar/1978157",
-        verification:"31ada151aee67435dbc5d04aa222ef8bc353a11b4941f39df3a5f36e9912cadb",
+    loadCalendarUrl:
+      "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978157",
+    verification:
+      "31ada151aee67435dbc5d04aa222ef8bc353a11b4941f39df3a5f36e9912cadb",
     laundry: 2,
     images: [
       { image_url: L3 },
@@ -100,17 +104,19 @@ export const apartmentList = [
       { image_url: L3131 },
     ],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
   {
     id: "smoobuApartment1978157de",
     name: "Townloft - am Borussia Park -L6-",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     bedroom: 3,
     bathroom: 2,
-    loadCalendarUrl:"https://login.smoobu.com/de/cockpit/widget/single-calendar/1978157",
-    verification:"31ada151aee67435dbc5d04aa222ef8bc353a11b4941f39df3a5f36e9912cadb",
-   
+    loadCalendarUrl:
+      "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978157",
+    verification:
+      "31ada151aee67435dbc5d04aa222ef8bc353a11b4941f39df3a5f36e9912cadb",
+
     images: [
       { image_url: L6 },
       { image_url: L61 },
@@ -119,21 +125,23 @@ export const apartmentList = [
       { image_url: L61616 },
     ],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
   {
     id: "smoobuApartment1978160de",
     name: "Townloft - am Borussia Park -L7-",
-    location: "Düsseldorf, Allemagne",
+    location: "Mönchengladbach, Germany",
     bedroom: 3,
     bathroom: 2,
-    loadCalendarUrl:"https://login.smoobu.com/de/cockpit/widget/single-calendar/1978160",
-    verification:"5a7b80431b6c4220efaa15fc31ddc30cdc87ead259f390514f97f98fa409070e",
+    loadCalendarUrl:
+      "https://login.smoobu.com/de/cockpit/widget/single-calendar/1978160",
+    verification:
+      "5a7b80431b6c4220efaa15fc31ddc30cdc87ead259f390514f97f98fa409070e",
     images: [
       { image_url: "https://picsum.photos/200/300" },
       { image_url: "https://picsum.photos/200/300" },
     ],
     description:
-      "Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities, Spacious luxury apartment with great amenities .",
+      "Über unsere Unterkunft:Ob Sie geschäftlich im Nordpark unterwegs sind, ein Heimspiel im Borussia-Park besuchen oder ein Konzert oder eine Veranstaltung genießen – wir möchten Ihnen ein zweites Zuhause bieten und hoffen, dass es Ihnen gefällt.Fühlen Sie sich wohl mit CityFlat",
   },
 ];
